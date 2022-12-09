31acd73db5a2408059b42aaf402d74f1830c5995
glitch-afk <rahul004@protonmail.ch>
2022-12-09 16:13:07 +0530

    taking input from console and printing it

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
     println!("Guessing Game!");
+
+    println!("Please input a guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line.");
+
+    println!("You guessed: {guess}");
 }
