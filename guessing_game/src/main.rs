14bbe65774d5f7c53871697134f32d2f2216fb04
glitch-afk <rahul004@protonmail.ch>
2022-12-09 16:26:14 +0530

    added rand crate to generate a random number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,13 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guessing Game!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("🤫 Secret Number : {secret_number}");
 
     println!("Please input a guess.");
 
@@ -9,7 +15,13 @@ fn main() {
 
     io::stdin()
         .read_line(&mut guess)
-        .expect("Failed to read line.");
+        .expect("⚠️ Failed to read line.");
 
-    println!("You guessed: {guess}");
+    let guess: u32 = guess.trim().parse().expect("⚠️ Please enter a number");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Greater => println!("Too Big"),
+        Ordering::Less => println!("Too small"),
+        Ordering::Equal => println!("You win! 🎉"),
+    }
 }
