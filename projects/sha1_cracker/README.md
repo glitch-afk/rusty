1dd41f5501da0a1a9aed79dce64cbc6fa2448aa1
glitch-afk <rahul004@protonmail.ch>
2022-12-13 18:32:43 +0530

    ✅ sha1 cracker

---
+++ projects/sha1_cracker/README.md
@@ -0,0 +1,7 @@
+**SHA1 HASH DECODER**
+
+```bash copy
+cargo run -- <wordlist> <hash_to_crack>
+```
+
+![output](result.png)
--- projects/sha1_cracker/src/main.rs
+++ projects/sha1_cracker/src/main.rs
@@ -39,7 +39,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 
-    println!("Password not found in wordlist :(");
+    println!("{}", "Password not found in wordlist :(".red());
 
     Ok(())
 }
